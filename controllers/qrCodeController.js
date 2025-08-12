76b997a8a6a4bfc5a20347db48170d32ab641f4c
Berk Certel <m.berkcertel@outlook.com>
2025-08-12 21:40:30 +0300

    rate limiter edited

--- controllers/qrCodeController.js
+++ controllers/qrCodeController.js
@@ -27,18 +27,15 @@ exports.createQrCode = async (req, res) => {
       label,
     });
     if (existingQr) {
-      return res
-        .status(409)
-        .json({
-          message:
-            "A QR code with this label already exists for this location.",
-        });
+      return res.status(409).json({
+        message: "A QR code with this label already exists for this location.",
+      });
     }
 
     const qrId = new mongoose.Types.ObjectId();
 
     // 1. Linki oluştur
-    const qrData = `${process.env.CLIENT_URL}/tr/menu/${qrId}`;
+    const qrData = `${process.env.CLIENT_URL}/tr/order/menu/${qrId}`;
 
     // 2. QR görselini oluştur
     const qrCodeImage = await QRCodeLib.toDataURL(qrData);
--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,10 +1,32 @@
-// backend/middleware/rateLimiter.js
+// // backend/middleware/rateLimiter.js
+// const rateLimit = require("express-rate-limit");
+
+// const apiLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 dakika
+//   max: 100, // Her IP için 100 istek
+//   message: "Çok fazla istek yaptınız. Lütfen daha sonra tekrar deneyin.",
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
+
+// module.exports = apiLimiter;
+
 const rateLimit = require("express-rate-limit");
 
 const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 dakika
-  max: 100, // Her IP için 100 istek
-  message: "Çok fazla istek yaptınız. Lütfen daha sonra tekrar deneyin.",
+  windowMs: 10 * 60 * 1000, // 10 dakika
+  max: 100,
+  handler: (req, res) => {
+    const now = Date.now();
+    const retryAfter = req.rateLimit?.resetTime
+      ? Math.ceil((req.rateLimit.resetTime - now) / 1000)
+      : 10 * 60;
+    res.set("Retry-After", retryAfter);
+    res.status(429).json({
+      message: "Çok fazla istek yaptınız. Lütfen daha sonra tekrar deneyin.",
+      retryAfter, // saniye cinsinden
+    });
+  },
   standardHeaders: true,
   legacyHeaders: false,
 });
