67658970a1eea8a24d607b8a05933a795172e720
Berk Certel <m.berkcertel@outlook.com>
2025-09-12 01:00:56 +0300

    ..

--- utils/getCookieOptions.js
+++ utils/getCookieOptions.js
@@ -1,10 +1,33 @@
+// // Cookie ayarlarını ortam bazlı döndüren fonksiyon
+// function getCookieOptions() {
+//   const isProd = process.env.NODE_ENV === "production" ? true : false;
+//   return {
+//     httpOnly: true,
+//     secure: isProd, // prod'da true, localde false
+//     sameSite: isProd ? "None" : "Lax",
+//     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 gün
+//     path: "/",
+//   };
+// }
+
+// // Auth cookie setle
+// function setAuthCookie(res, token) {
+//   res.cookie("token", token, getCookieOptions());
+// }
+
+// // Auth cookie sil (logout)
+// function clearAuthCookie(res) {
+//   res.clearCookie("token", getCookieOptions());
+// }
+
+// module.exports = { setAuthCookie, clearAuthCookie };
+
 // Cookie ayarlarını ortam bazlı döndüren fonksiyon
 function getCookieOptions() {
-  const isProd = process.env.NODE_ENV === "production" ? true : false;
   return {
     httpOnly: true,
-    secure: isProd, // prod'da true, localde false
-    sameSite: isProd ? "None" : "Lax",
+    secure: true, // prod'da true, localde false
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 gün
     path: "/",
   };
